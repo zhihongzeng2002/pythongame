6e44d0572ae5b8ad1cd2086e8b9b8fc92ba283bd
Zhihong Zeng <zhihong.zeng@parkassist.com>
2018-01-06 21:12:10 -0500

    add cipher game

--- converttxt2py.py
+++ converttxt2py.py
@@ -1,19 +1,19 @@
 import os, sys
 
-def main(srcfile, desfile):
+def main(srcfile, desfile, n_space):
     with open(srcfile) as readfh:
         with open(desfile, 'w') as writefh:
             content = readfh.readlines()
 
             for x in content:
-                line = x[4:]
+                line = x[n_space:]
                 writefh.write(line)
                 
 
 
 
 if __name__ == '__main__':
-    if len(sys.argv) != 3:
-        print 'Usage: %s srctxt despy' % sys.argv[0]
+    if len(sys.argv) != 4:
+        print 'Usage: %s srctxt despy space_number' % sys.argv[0]
     else:
-        main(sys.argv[1], sys.argv[2])
+        main(sys.argv[1], sys.argv[2], int(sys.argv[3]))
