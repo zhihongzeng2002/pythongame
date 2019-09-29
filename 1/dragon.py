176d798b934441029f309389de8bfe98ea5b0a31
Zhihong Zeng <zhihongzeng2002@gmail.com>
2019-09-29 12:03:09 -0400

    add pythongame_1_dragonrealm.pdf

--- 1/dragon.py
+++ 1/dragon.py
@@ -5,31 +5,24 @@ import random
 import time
 
 def displayIntro():
-    print('You are in a land full of dragons. In front of you,')
-    print('you see two caves. In one cave, the dragon is friendly')
-    print('and will share his treasure with you. The other dragon')
-    print('is greedy and hungry, and will eat you on sight.')
-    print()
+    print('''you see two caves.
+            In one cave, the dragon is friendly. 
+            In the other cave, the other dragon is greedy and hungry.
+            ''')
 
 def chooseCave():
     cave = ''
     while cave != '1' and cave != '2':
-        print('Which cave will you go into? (1 or 2)')
-        cave = input()
+        cave = input('Which cave will you go into? (1 or 2) ')
 
     return cave
 
 def checkCave(chosenCave):
-    print('You approach the cave...')
-    time.sleep(2)
-    print('It is dark and spooky...')
-    time.sleep(2)
-    print('A large dragon jumps out in front of you! He opens his jaws and...')
-    print()
+    print('A large dragon jumps out in front of you! He opens his jaws and...\n')
     time.sleep(2)
     
-    friendlyCave = random.randint(1, 2)
-    if chosenCave == str(friendlyCave):
+    number = random.randint(1, 2)
+    if chosenCave == str(number):
         print('Gives you his treasure!')
     else:
         print('Gobbles you down in one bite!')
@@ -39,5 +32,5 @@ while playAgain == 'yes' or playAgain == 'y':
     displayIntro()
     caveNumber = chooseCave()
     checkCave(caveNumber)
-    print('Do you want to play again? (yes or no)')
-    playAgain = input()        
+    print()
+    playAgain = input('Do you want to play again? (yes or no)')        
