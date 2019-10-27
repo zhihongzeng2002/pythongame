a68bf9250f692be706de6373f05c1e619ec090d4
Zhihong Zeng <zhihongzeng2002@gmail.com>
2019-10-27 13:35:25 -0400

    add tictactoe game, including pdf and code

---
+++ 1/tictactoe_2019.py
@@ -0,0 +1,142 @@
+# Zhihong Zeng
+# 10/26/2019
+
+import random
+import numpy as np
+
+
+def create_board(size, value=' '):
+    board = np.full((size,size), value)
+    return board
+
+def input_player_selection():
+    letter = ''
+    while letter not in ['X', 'O']:
+        letter = input('Do you want to be X or O?')
+        letter = letter.upper()
+    if letter == 'X':
+        return ['X', 'O']
+    else:
+        return ['O', 'X']
+
+def who_go_first():
+    ans = random.randint(0,1)
+    if ans:
+        return 'computer'
+    else:
+        return 'player'
+
+def make_move(board, letter, position):
+    board[position[0], position[1]] = letter
+
+def column_won(board, letter):
+    height, width = board.shape
+    for x in range(width):
+        won = True
+        for y in range(height):
+            if board[y, x] != letter:
+                won = False
+                break
+        if won:
+            return True
+    return False
+
+def row_won(board, letter):
+    height, width = board.shape
+    for y in range(height):
+        won = True
+        for x in range(width):
+            if board[y, x] != letter:
+                won = False
+                break
+        if won:
+            return True
+    return False
+
+def diag_won(board, letter):
+    height, width = board.shape
+    won = True
+    for y in range(height):
+        if board[y, y] != letter:
+            won = False
+            break
+    if won:
+        return True
+
+    won = True
+    for y in range(height):
+        if board[y, height-1-y] != letter:
+            won = False
+            break
+
+    return won
+
+def game_won(board, letter):
+##    print(column_won(board, letter), row_won(board, letter), diag_won(board, letter))
+    return column_won(board, letter) \
+           or row_won(board, letter) \
+           or diag_won(board, letter)
+
+def get_available_move(board):
+    height, width = board.shape
+    move = []
+    for y in range(height):
+        for x in range(width):
+            if board[y, x] == ' ':
+                move.append([y, x])
+    return move
+    
+def get_player_move(board, letter):
+    height, width = board.shape
+    available_move = get_available_move(board)
+    move = (-1, -1)
+    while move not in available_move:
+        ans = input('What is your next move ? (row (0-{}), column(0-{})) '.format(height, width))
+        move = ans.split(',')
+        move = list(map(int, move))
+    make_move(board, letter, move)
+
+def get_random_move(board, letter):
+    print('computer move')
+    available_move = get_available_move(board)
+    move = random.choice(available_move)
+    make_move(board, letter, move)
+
+def tictactoe():
+    print('Welcom to Tic-Tac-Toe!')
+    player, computer = input_player_selection()
+    print('You are {}, and computer is {}'.format(player, computer))
+    first = who_go_first()
+    print('{} go first'.format(first))
+    board = create_board(3)
+    turn = first
+    while get_available_move(board):
+        if turn == 'player':
+            get_player_move(board, player)
+            print(board)
+            turn = 'computer'
+            if game_won(board, player):
+                print('Congratulations. You won the game')
+                return
+        else:
+            get_random_move(board, computer)
+            print(board)
+            turn = 'player'
+            if game_won(board, computer):
+                print('The computer won the game')
+                return
+    print('The game is a tie')
+        
+if __name__ == '__main__':
+    tictactoe()    
+    
+
+
+        
+        
+            
+
+
+
+
+
---
+++ 1/tictactoe_2019_test.py
@@ -0,0 +1,109 @@
+from tictactoe_2019 import *
+
+print('\n-----Test create_board\n')
+print(create_board(3))
+print(create_board(5))
+
+print('\n-----test input_player_letter\n')
+print(input_player_selection())
+
+print('\n-----test who_go_first\n')
+print(who_go_first())
+print(who_go_first())
+print(who_go_first())
+
+print('\n-----test make_move\n')
+board = create_board(3)
+make_move(board, 'X', (0, 0))
+print(board)
+make_move(board, 'O', (2, 2))
+print(board)
+
+print('\n-----test column_won\n')
+board = create_board(3)
+print(board)
+ans = column_won(board, 'X')
+assert not ans, 'Fail'
+board[0, 0] = 'X'
+board[1, 0] = 'X'
+board[2, 0] = 'X'
+print(board)
+ans = column_won(board, 'X')
+assert ans, 'Fail'
+
+print('\n-----test row_won\n')
+board = create_board(3)
+print(board)
+ans = row_won(board, 'X')
+assert not ans, 'Fail'
+board[1, 0] = 'X'
+board[1, 1] = 'X'
+board[1, 2] = 'X'
+print(board)
+ans = row_won(board, 'X')
+assert ans, 'Fail'
+
+print('\n-----test diag_won\n')
+board = create_board(3)
+print(board)
+ans = diag_won(board, 'X')
+assert not ans, 'Fail'
+board[0, 0] = 'X'
+board[1, 1] = 'X'
+board[2, 2] = 'X'
+print(board)
+ans = diag_won(board, 'X')
+assert ans, 'Fail'
+
+board = create_board(3)
+print(board)
+ans = diag_won(board, 'X')
+assert not ans, 'Fail'
+board[0, 2] = 'X'
+board[1, 1] = 'X'
+board[2, 0] = 'X'
+print(board)
+ans = diag_won(board, 'X')
+assert ans, 'Fail'
+
+print('\n------test game_won\n')
+board = create_board(3)
+ans = game_won(board, 'X')
+assert not ans, 'Fail'
+board[0, 2] = 'X'
+board[1, 2] = 'X'
+board[2, 2] = 'X'
+ans = game_won(board, 'X')
+assert ans, 'Fail'
+print('\n==== Success====\n')
+
+print('\n-----test get_available_move\n')
+board = create_board(3)
+board[0, 0] = 'X'
+board[1, 1] = 'O'
+board[2, 2] = 'O'
+ans = get_available_move(board)
+print(board)
+print('available_move: {}'.format(ans))
+
+print('\n-----test get_player_move\n')
+board = create_board(3)
+print(board)
+get_player_move(board, 'X')
+print(board)
+
+print('\n--------test get_random_move\n')
+board = create_board(3)
+get_random_move(board, 'O')
+print(board)
+board = create_board(3)
+get_random_move(board, 'O')
+print(board)
+print('\n==== Success====\n')
+
+
+    
+
+      
+
+
