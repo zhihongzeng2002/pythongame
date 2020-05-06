b717437e95af8f266ec2a0eb1247ae75e2df791d
Zhihong(John) Zeng <zhihong.zeng@ironmountain.com>
2020-05-06 14:11:59 -0400

    add optimization.py

--- mit/datascience/optimization.py
+++ mit/datascience/optimization.py
@@ -13,6 +13,29 @@ def optimal_score(toConsider, avail, score):
         else:
             return (withoutVal, withoutToTake)
 
+def lookup_update(toConsider, avail, score, memo, func):
+    key = (len(toConsider), avail, score)
+    if key in memo:
+        return memo[key]
+    else:
+        memo[key] = func(toConsider, avail, score, memo)
+        return memo[key]
+
+def optimal_score_dynamicprograming(toConsider, avail, score, memo):
+    print(toConsider, avail, score)
+    if avail <= 20 or not toConsider:
+        return (score, toConsider)
+    else:
+        nextItem = toConsider[0]
+        withVal, withToTake = lookup_update(toConsider[1:], avail, score, memo)
+        withoutVal, withoutToTake = lookup_update(toConsider[1:], avail-10, score-nextItem)
+
+        if withVal >= withoutVal:
+            return (withVal, withToTake + [nextItem])
+        else:
+            return (withoutVal, withoutToTake)
+
+
 # Score	=	((60	–	(a+b+c+d+e))*F	+	a*ps1	+	b*ps2	+	c*ps3	+	d*ps4	+	e*ps5
 #       = 60*F + (-F + ps1)*a + (-F + ps2)*b + (-F + ps3)*c + (-F + ps4)*d + (-F + ps5)*e
 
@@ -35,5 +58,9 @@ for x in items:
 
 print('base score: {}'.format(score))
 max_score = optimal_score(items, len(items) * 10, score)
+print(max_score)
+
+print('\ndynamic programing')
+max_score = optimal_score_dynamicprograming(items, len(items) * 10, score, {})
 print(max_score)
 
