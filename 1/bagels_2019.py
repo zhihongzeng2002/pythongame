3ebd6497a648c32a36673fe0124a5ed3452d5fd0
zhihongzeng2002 <zhihongzeng2002@gmail.com>
2021-01-10 17:32:40 -0500

    update bagels games pdf and code

--- 1/bagels_2019.py
+++ 1/bagels_2019.py
@@ -17,7 +17,7 @@ def get_random_number(size):
 def get_player_input(size):
     ans = ''
     while len(ans) != size or not ans.isdigit():
-        ans = input('Make a guess ({} digits): \n'.format(size))
+        ans = input(f'Make a guess ({size} digits): \n')
     return ans
 
 def get_clue(guess, secrete_number):
@@ -34,19 +34,19 @@ def get_clue(guess, secrete_number):
     if not clue:
         return 'Bagels'
     else:
-        # clue.sort()
         return ' '.join(clue)
 
 def introduction():
-    intro = '''
-    I am thinking of a {}-digit number. Try to guess what it is.
+    intro = f'''
+    I am thinking of a {NUM_DIGIT}-digit number. Try to guess what it is.
     The clues I give are...
     When I say:    That means:
     Bagels       None of the digits is correct.
     Pico         One digit is correct but in the wrong position.
     Fermi        One digit is correct and in the right position.
-    I have thought up a number. You have {} guesses to get it.
-    '''.format(NUM_DIGIT, MAX_GUESS)
+    I have thought up a number. You have {MAX_GUESS} guesses to get it.
+    '''
+    
     print(intro)
 
 def bagels_game():
@@ -54,7 +54,7 @@ def bagels_game():
     secrete = get_random_number(NUM_DIGIT)
 
     for i in range(MAX_GUESS):
-        print('\n#{}:'.format(i+1))
+        print(f'\n#{i+1}:')
         ans = get_player_input(NUM_DIGIT)
         clue = get_clue(ans, secrete)
         if clue == 'Won':
