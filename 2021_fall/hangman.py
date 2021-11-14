bb206dc32f0b95ac5c866f04bcdc92425ed40b18
zhihongzeng2002 <zhihongzeng2002@gmail.com>
2021-11-14 16:58:11 -0500

    update pythongame_1_hangman.pdf and 2021_fall/hangman.py and hangman_test.py

--- 2021_fall/hangman.py
+++ 2021_fall/hangman.py
@@ -43,6 +43,44 @@ def guess_loop(secrete_word, max_guess):
             print(f'Oops. That letter is not in my word: {guessed_word}')
             max_guess -= 1
     print(f'Sorry, you ran out of guesses. My word is {secrete_word}')
+
+from hangman_pictures import HANGMANPICS
+
+def guess_loop_2(secrete_word): ###
+    max_guess = len(HANGMANPICS) ###
+    guessed = ''
+    while max_guess > 1:  ###
+        print(f'You have {max_guess-1} guessed left') ###
+        print(HANGMANPICS[-max_guess])  ###
+        letter = input('Please guess a letter: ').lower()
+        if letter in secrete_word:
+            if letter in guessed:
+                print('That letter has already been guessed')
+            else:
+                guessed += letter
+                guessed_word = get_guessed_word(secrete_word, guessed)
+                print(f'Good guess: {guessed_word}')
+
+                if is_word_guessed(secrete_word, guessed):
+                    print('Congrats, you won.\n')
+                    return
+
+        else:
+            guessed_word = get_guessed_word(secrete_word, guessed)
+            print(f'Oops. That letter is not in my word: {guessed_word}')
+            max_guess -= 1
+    print(HANGMANPICS[-1])  ###
+    print(f'Sorry, you ran out of guesses. My word is {secrete_word}')
+
             
+def hangman(): ###
+    secrete_word = choose_word()
+    print('Welcome the game')
+    guess_loop_2(secrete_word)   ###
+
+if __name__ == '__main__':
+    hangman()
+
+    
             
 
--- 2021_fall/hangman_test.py
+++ 2021_fall/hangman_test.py
@@ -1,25 +1,28 @@
 from hangman import *
 
-w = choose_word()
-print(w)
+##w = choose_word()
+##print(w)
+##
+##w = choose_word()
+##print(w)
+##
+##ans = is_word_guessed('beer', 'ber')
+##assert ans == True, 'is_word_guessed failed'
+##
+##ans = is_word_guessed('beer', 'be')
+##assert ans == False, 'is_word_guessed failed'
+##
+##print('is_word_guessed successed')
+##
+##ans = get_guessed_word('hangman', 'an')
+##assert ans == '_an__an', 'get_guessed_word failed'
+##
+##ans = get_guessed_word('beer', 'be')
+##assert ans == 'bee_', 'get_guessed_word failed'
+##
+##print('get_guessed_word successed')
+##
+##guess_loop('hangman', 3)
 
-w = choose_word()
-print(w)
+hangman(3)
 
-ans = is_word_guessed('beer', 'ber')
-assert ans == True, 'is_word_guessed failed'
-
-ans = is_word_guessed('beer', 'be')
-assert ans == False, 'is_word_guessed failed'
-
-print('is_word_guessed successed')
-
-ans = get_guessed_word('hangman', 'an')
-assert ans == '_an__an', 'get_guessed_word failed'
-
-ans = get_guessed_word('beer', 'be')
-assert ans == 'bee_', 'get_guessed_word failed'
-
-print('get_guessed_word successed')
-
-guess_loop('hangman', 3)
