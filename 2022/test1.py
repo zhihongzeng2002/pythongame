0e382a46af86e67dbce71578cbb4c0ec82e0b2c0
zhihongzeng2002 <zhihongzeng2002@gmail.com>
2022-02-27 16:51:38 -0500

    add test1.py and test2.py

---
+++ 2022/test1.py
@@ -0,0 +1,13 @@
+x = float(input('Input x: '))
+y = float(input('Input y: '))
+
+print(f'x={x}, y={y}')
+
+if x==y:
+    print('x == y')
+elif x > y:
+    print('x > y')
+else:
+    print('x < y') 
+
+print('Good job')
---
+++ 2022/test2.py
@@ -0,0 +1,6 @@
+x = input('You are in the forest. You choose: right or left? ')
+
+while x == 'right':
+    x = input('choose again: right or left? ')
+
+print('Congrats. You got out of the forest')
