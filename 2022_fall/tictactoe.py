12d2fa5b4546bea13e1263b95fc7fe9f76127968
Andrew Zeng <andrewzeng@lava-tube--33.dynamic.rpi.edu>
2022-11-13 16:50:35 -0500

    updated tictactoe code

--- 2022_fall/tictactoe.py
+++ 2022_fall/tictactoe.py
@@ -1,5 +1,46 @@
 import numpy as np
+import random
 
 def create_board(size, value = ''):
     board = np.full((size, size), value)
     return board
+
+def input_player_selection():
+    letter = ''
+    while letter not in ['X', 'O']:
+        letter = input('Do you want to be X or O?').upper()
+    
+    if letter == 'X':
+        return ['X', 'O']
+    else:
+        return ['O', 'X']
+
+def who_go_first():
+    ans = random.randint(0,1)
+    if ans:
+        return 'computer'
+    else:
+        return 'player'
+
+def make_move(board, letter, position):
+    board[position[0], position[1]] = letter
+
+def get_available_move(board):
+    height, width = board.shape
+    move = []
+    for y in range(height):
+        for x in range(width):
+            if board[y, x] == ' ':
+                move.append([y, x])
+    return move
+
+def get_player_move(board, letter):
+    height, width = board.shape
+    available_move = get_available_move(board)
+    move = (-1, -1)
+
+    while move not in available_move:
+        ans = input(f'What is your next move (0-{height-1}, 0-{width-1})?')
+        move = ans.split(',')
+    
+    make_move(board,letter,move)
--- 2022_fall/tictactoe_test.py
+++ 2022_fall/tictactoe_test.py
@@ -1,4 +1,31 @@
 from tictactoe import *
 
 print('----Test create board fuction\n')
-print(create_board(3, ' '))
+board = create_board(3, ' ')
+print(board)
+
+# print('----Test input player selection\n')
+# print(input_player_selection())
+
+print('----Test who goes first\n')
+print(who_go_first())
+print(who_go_first())
+print(who_go_first())
+
+print('----Test making a move\n')
+make_move(board, 'X', (0, 0))
+print(board)
+make_move(board, 'O', (2, 2))
+print(board)
+
+print('----Test get available move\n')
+board[1,1] = 'O'
+board[1,2] = 'X'
+board[0,1] = 'O'
+ans = get_available_move(board)
+print(board)
+print('available moves: {}'.format(ans))
+
+print('----Test get player move\n')
+board = create_board(3, ' ')
+print(board)
