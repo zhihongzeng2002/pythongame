18b74fcad926e04a3546591783eb16bd08779340
Andrew Zeng <andrewzeng@cardinal-697.dynamic.rpi.edu>
2024-02-27 10:05:24 -0500

    updated hello world code

--- 2024_spring/helloworld.py
+++ 2024_spring/helloworld.py
@@ -1,14 +1,22 @@
 import pygame, sys
 from pygame.locals import QUIT
 
+speed = [1,1]
+width, height = 800, 600
+
 pygame.init()
 
-pygame.display.set_mode((400, 300))
+screen = pygame.display.set_mode((width, height))
 pygame.display.set_caption("This is a surface")
+
+ball = pygame.image.load('831.png')
+ballrect = ball.get_rect()
 
 while True:
     for event in pygame.event.get():
         if event.type == QUIT:
             pygame.quit()
             sys.exit()
+
+    screen.blit(ball, ballrect)
     pygame.display.update()
