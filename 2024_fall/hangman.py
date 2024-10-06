17b31887667b656196b1ebf2a5a003574e59a3b7
Andrew Zeng <andrewzeng@Andrews-Air-3.fios-router.home>
2024-10-06 16:48:39 -0400

    finished hangman

--- 2024_fall/hangman.py
+++ 2024_fall/hangman.py
@@ -1,17 +1,37 @@
 import random
+from hangman_pictures import HANGMANPICS
+
+def blankSpace(s, g):
+    out = ''
+    
+    for x in s:
+        if x in g:
+            out += x
+        else:
+            out += "_"
+
+    return out
 
 posWords = ['sigma', 'delaware', 'paper', 'spiral', 'butter']
 secret = random.choice(posWords)
 
 guessed = ''
-guesses = 5
+guesses = 7
 
 while guesses > 0:
+    print("Number of guesses left: " + str(guesses))
+    print(HANGMANPICS[7 - guesses])
+    
     g = input("Please guess a letter: ")
     guessed += g
     #check if guess is valid
     if g in secret:
         print("Letter was in the word")
+
+        #print out the blanks
+        out = blankSpace(secret, guessed)
+                
+        print(out)
 
         #check if all letters in word have been guessed
         won = True
@@ -26,6 +46,11 @@ while guesses > 0:
     else:
         print("Letter was not in the word")
         guesses -= 1
+
+        #print out the blanks
+        out = blankSpace(secret, guessed)
+                
+        print(out)
 
     print("The letters guessed so far: " + guessed)
 
