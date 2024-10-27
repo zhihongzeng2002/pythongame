c01cef04a4233ddca7a534daa16678f7b71face1
Andrew Zeng <andrewzeng@Andrews-Air-3.fios-router.home>
2024-10-27 17:55:19 -0400

    added win conditions

--- 2024_fall/tictactoe.py
+++ 2024_fall/tictactoe.py
@@ -15,3 +15,57 @@ def pickLetter():
 def printBoard(board):
     for row in board:
         print(row)
+
+def checkRow(board, row, letter):
+    won = True
+    for x in range(3):
+        if board[row][x] != letter:
+            won = False
+
+    return won
+
+def checkRows(board, letter):
+    for row in range(3):
+        won = checkRow(board, row, letter)
+        if won == True:
+            return True
+
+    return False
+
+def checkCol(board, col, letter):
+    won = True
+    for x in range(3):
+        if board[x][col] != letter:
+            won = False
+
+    return won
+
+def checkCols(board, letter):
+    for col in range(3):
+        won = checkCol(board, col, letter)
+        if won == True:
+            return True
+
+    return False
+
+def checkDiag(board, letter):
+    won = True
+    for i in range(3):
+        if board[i][i] != letter:
+            won = False
+
+    if won == True:
+        return won
+
+    won = True
+    for i in range(3):
+        if board[i][2-i] != letter:
+            won = False
+
+    return won
+
+def checkWin(board, letter):
+    if (checkRows(board, letter) or checkCols(board, letter)\
+        or checkDiag(board, letter)):
+        return True
+    return False
--- 2024_fall/tictactoetest.py
+++ 2024_fall/tictactoetest.py
@@ -5,7 +5,37 @@ print(x)
 
 printBoard(x)
 
-a = pickLetter()
-print(a)
-a = pickLetter()
-print(a)
+#a = pickLetter()
+#print(a)
+#a = pickLetter()
+#print(a)
+
+
+print(checkRows(x, "x"))
+print(checkRows(x, "o"))
+
+x = [["x", "x", "x"],["", "", ""],["o", "o", "o"]]
+printBoard(x)
+print(checkWin(x, "x"))
+print(checkWin(x, "o"))
+
+x = [["x", "o", "x"],["", "", ""],["o", "o", "o"]]
+printBoard(x)
+print(checkWin(x, "x"))
+
+x = [["x", "x", "o"],["x", "", "o"],["x", "o", "o"]]
+printBoard(x)
+print(checkWin(x, "x"))
+print(checkWin(x, "o"))
+
+x = [["x", "o", "x"],["", "", ""],["o", "o", "o"]]
+printBoard(x)
+print(checkWin(x, "x"))
+
+x = [["x", "o", "x"],["", "x", ""],["o", "o", "x"]]
+printBoard(x)
+print(checkWin(x, "x"))
+
+x = [["x", "o", "o"],["", "o", ""],["o", "o", "o"]]
+printBoard(x)
+print(checkWin(x, "o"))
