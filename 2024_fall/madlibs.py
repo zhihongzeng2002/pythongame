5514e27458ab0e16c77d27178cfb126b41741e7c
Andrew Zeng <andrewzeng@Andrews-Air-3.lan>
2025-01-05 16:55:05 -0500

    finished mad libs

--- 2024_fall/madlibs.py
+++ 2024_fall/madlibs.py
@@ -1,11 +1,18 @@
 def madlibs():
     inputFile = "test.txt"
+    outputFile = "output.txt"
     
     file = open(inputFile, "r")
     text = file.read()
     words = text.split()
     file.close()
 
-    print(words)
+    file = open(outputFile, "w")
+    for w in words:
+        #if w is (adjective) or (noun) or (verb)
+            #ask for w and write it
+        #else
+            file.write(w + " ")
+    file.close()
 
 madlibs()
